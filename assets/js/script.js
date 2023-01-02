04467a0e7fda3d9e7b32c298a891459f25f4cebe
cecilegaudron <ce.gaudron@gmail.com>
2023-01-02 12:35:50 +0000

    Add JavaScript function for Numbers Animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 /*
-Script fot the Current Year in the copyright in the footer
+Script for the Current Year in the Copyright in the Footer
 https://www.w3schools.com/js/js_date_methods.asp
 */
 let d = new Date();
 document.getElementById("year").innerHTML = d.getFullYear();
+
+/*
+Script for Animations to Numbers. OnMouseOver Rounds are Bigger
+https://www.w3schools.com/jsref/event_onmouseover.asp
+*/
+
+function bigRound(x) {
+    x.style.height = "80%";
+    x.style.width = "80%";
+}
+
+function normalRound(x) {
+    x.style.height = "60%";
+    x.style.width = "60%";
+}
