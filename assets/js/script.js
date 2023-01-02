8b9bb02d1dec3e8aeacbeec334646b4df46853d1
cecilegaudron <ce.gaudron@gmail.com>
2023-01-02 09:57:25 +0000

    Add JS Script for the Current Year in the Footer

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/*
+Script fot the Current Year in the copyright in the footer
+https://www.w3schools.com/js/js_date_methods.asp
+*/
+let d = new Date();
+document.getElementById("year").innerHTML = d.getFullYear();
