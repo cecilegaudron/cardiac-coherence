5d253a1d7a7bd044581a851b20a78095e89c9809
cecilegaudron <ce.gaudron@gmail.com>
2023-01-02 14:26:54 +0000

    Try New Function but not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ document.getElementById("year").innerHTML = d.getFullYear();
 /*
 Script for Animations to Numbers. OnMouseOver Rounds are Bigger
 https://www.w3schools.com/jsref/event_onmouseover.asp
-*/
+
 
 function bigRound(x) {
     x.style.height = "80%";
@@ -19,4 +19,21 @@ function normalRound(x) {
     x.style.height = "60%";
     x.style.width = "60%";
     x.style.display = false;
+    document.getElementsById('first-hide') = style.display('False');
 }
+*/
+
+/* Script for Animations to Numbers
+*/
+function revealFirstText() {
+    let x = document.getElementsById("first-hide");
+    x.style.height = "80%";
+}
+    /*
+    if (firstText.style.display === "none") {
+        firstText.style.display = "block";
+    } else {
+        firstText.display = "none";
+    }
+}
+*/
