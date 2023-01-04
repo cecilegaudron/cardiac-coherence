f068f9f40f7180fc515c9c364b4fab6743cab185
cecilegaudron <ce.gaudron@gmail.com>
2023-01-04 15:17:39 +0000

    Petals grow and Shrink without Loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,31 +13,35 @@ Script for Animated Petals
 document.getElementById("start").addEventListener("click", startAnimation);
 
 let myPetals = document.getElementById("petals");
-let petalsWidth = myPetals.clientWidth;
-const bigSize = "350px";
-const smallSize = "50px";
+//let petalsWidth = myPetals.clientWidth;
+//let bigSize = setTimeout(goToBigSize.apply(), 2000);
+//let smallSize = setTimeout(goToSmallSize.apply(), 2000);
 
+// Function to Go to the Big Size
 function goToBigSize() {
     myPetals.style.width = "350px";
+    console.log("fnction go to BIG size OKAY");
 }
 
+// Function to Go to the Small Size
 function goToSmallSize() {
     myPetals.style.width = "50px";
+    console.log("fnction go to SMALL size OKAY");
 }
 
+// Function to Start the Animation with Big Size - Wait - Go to Small Size
 function startAnimation() {
-    goToBigSize.apply();
+    goToBigSize.apply(); 
+    console.log("gotobigsize OKAY");
+
+    setTimeout(goToSmallSize, 8000); 
+    console.log("gotosmallsize OKAY");
+
+    //setInterval(goToBigSize.apply(), 8000); 
+    //console.log("go to big size again OKAY");
+
 }
 
-function animationLoop() {
-    if (petalsWidth < bigSize) {
-        //goToBigSize.apply();
-        alert("go to 350");
-    } else {
-        //goToSmallSize.apply();
-        alert(" go to 50!");
-    }
-}
 
 /* Script for Stopping Animated Petals
 https://www.w3schools.com/js/js_timing.asp
@@ -45,11 +49,6 @@ https://www.w3schools.com/js/js_timing.asp
 document.getElementById("stop").addEventListener("click", stopAnimation);
 
 function stopAnimation() {
-    //alert("Cia Cruella!");
-    document.getElementById("petals").style.width = "100px";
+    console.log("Stop the animation");
+    clearInterval();
   }
-/* SCRIPT OKAY WITH MOUSE OVER 
-  function myFunction() {
-    document.getElementById("petals").style.transitionDuration = "1s";
-  }
-*/
